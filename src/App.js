616f82c3a38b02258fae059a250bf94153894bab
Trust Okerezi <trustokerezi42@gmail.com>
2022-12-21 12:08:04 +0100

    record table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Converter from './CryptoConverter/Converter';
+import RecordTable from './CryptoConverter/RecordTable';
 
 function App() {
   return (
     <div className="App">
       <Converter/>
+      <RecordTable/>
     </div>
   );
 }
